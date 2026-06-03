e38db9b471ecfd48f6111c7cd5460fc43e46344b
Alfredoooh <alfredopjonas@gmail.com>
2026-06-03 10:51:47 +0100

    update

--- app/src/main/java/com/ipc/app/ui/MyCoinActivity.kt
+++ app/src/main/java/com/ipc/app/ui/MyCoinActivity.kt
@@ -1,12 +1,12 @@
 package com.ipc.app.ui
 
 import android.os.Bundle
-import com.ipc.app.ui.BaseActivity
+import com.ipc.app.R
 
 class MyCoinActivity : BaseActivity() {
 
-override fun onCreate(savedInstanceState: Bundle?) {
-super.onCreate(savedInstanceState)
-// TODO: adicionar layout quando estiver pronto
-}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_coin)
+    }
 }
