8dda1fe9ca5e6e0d0900e2fd60e10c86103b4ce6
Alfredoooh <alfredopjonas@gmail.com>
2026-06-03 11:14:31 +0100

    update MainActiviy.kt

--- app/src/main/java/com/ipc/app/MainActiviy.kt
+++ app/src/main/java/com/ipc/app/MainActiviy.kt
@@ -56,9 +56,9 @@ class MainActiviy : BaseActivity() {
     private val SWIPE_EDGE_WIDTH = 40f
     private val SWIPE_MIN_DIST = 30f
 
-    private val MARGIN_NORMAL_DP   = 10f
+    private val MARGIN_NORMAL_DP    = 10f
     private val MARGIN_COLLAPSED_DP = 28f
-    private val RADIUS_NORMAL_DP   = 20f
+    private val RADIUS_NORMAL_DP    = 20f
     private val RADIUS_COLLAPSED_DP = 32f
 
     private val activeIconColor: Int
